3b65e557cb45d9ebf4e2917788bdaab571264162
njinjiaustin83-prog <njinjiaustin83@gmail.com>
2026-08-13 09:55:40 +1000

    Create backend/database.js

---
+++ backend/database.js
@@ -0,0 +1,10 @@
+"better-sqlite3": "^12.4.1"
+"dependencies": {
+  "bcryptjs": "^3.0.2",
+  "better-sqlite3": "^12.4.1",
+  "cors": "^2.8.5",
+  "dotenv": "^16.6.1",
+  "express": "^5.1.0",
+  "jsonwebtoken": "^9.0.2",
+  "nodemailer": "^7.0.5"
+}feat: add SQLite database dependency
